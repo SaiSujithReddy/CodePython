0459f9103e463c76c5291168f69f579709a15c08
Sai MarapaReddy <sai.marapareddy@gmail.com>
2018-03-29 19:14:11 -0700

    Adding more solutions solved through python
    Signed-off-by: Sai MarapaReddy <sai.marapareddy@gmail.com>
    Signed-off-by: saimarapareddy <sai.marapareddy@gmail.com>

--- ArrayRotatedSearch.py
+++ ArrayRotatedSearch.py
@@ -19,40 +19,72 @@ class Solution:
         :type target: int
         :rtype: int
         """
-        pivot = 0
+        if len(nums) > 0:
+            pivot = self.find_pivot(nums,0,len(nums)-1)
+            print("pivot is ",pivot)
+            print("pivot element is ", nums[pivot])
+            if nums[0] == target:
+                return 0
+            elif nums[len(nums) - 1] == target:
+                return len(nums) - 1
+            elif nums[pivot] == target:
+                return pivot
+
+        else:
+            return -1
+
+    def search_inside(self, nums, target):
+        if nums[0] == target:
+            return 0
+        elif nums[len(nums)-1] == target:
+            return len(nums)-1
+        elif nums[pivot] == target:
+            return pivot
+        else:
+            if nums[0]
+
+
 
     def find_pivot(self,nums,index1,index2):
         #for x in range(len(nums))
 
-        if len(nums) ==1:
+        # print("elemnts are", nums)
+        # print("index1 element is {} and index is {}",nums[index1],index1)
+        # print("index2 element is {} and index is {}", nums[index2], index2)
+
+        if len(nums[index1:(index2)+1]) ==1:
             return index1
-        elif len(nums) ==2:
+        elif len(nums[index1:(index2)+1]) ==2:
             if nums[index1] < nums[index2]:
                 return index2
             else:
                 return index1
         else:
-            if nums[index1] < nums[index2]:
-                if nums[index1] < nums[index2]:
+
+                if nums[index1] > nums[math.ceil((index2+index1)/2)] and nums[math.ceil((index2+index1)/2)] < nums[index2]:
+                    #print("Hi1")
+                    return self.find_pivot(nums,index1,math.ceil((index2+index1)/2))
+                elif nums[index1] < nums[math.ceil((index2+index1)/2)] and nums[math.ceil((index2+index1)/2)] > nums[index2]:
+                    #print("Hi2")
+                    return self.find_pivot(nums,math.floor((index2+index1)/2),index2)
+                elif nums[index1] < nums[math.ceil((index2+index1)/2)] and nums[math.ceil((index2+index1)/2)] < nums[index2]:
+                    #print("Hi3")
                     return index2
-                else:
+                elif nums[index1] > nums[math.ceil((index2+index1)/2)] and nums[math.ceil((index2+index1)/2)] > nums[index2]:
+
+                    # print("value of nums[index1] is",nums[index1] )
+                    # print("value of nums[index2] is", nums[index2])
+                    # print("value of nums[math.ceil((index2+index1)/2)] is", nums[math.ceil((index2+index1)/2)])
+                    # print("Hi4")
                     return index1
-            else:
 
-                if nums[index1] > nums[math.ceil((index2-index1)/2)] and nums[math.ceil((index2-index1)/2)] >
-
-                if nums[index1] > nums[math.ceil((index2-index1)/2)] < nums[index2]:
-                    return self.find_pivot(nums,math.floor((index2-index1)/2),index2)
-                else:
-                    return self.find_pivot(nums,index1,math.ceil((index2-index1)/2))
 
 
 
 list_integers = [4,5,6,7,8,9,10,1,2,3]
 sol=Solution()
-index = sol.find_pivot(list_integers,0,len(list_integers)-1)
-print(index)
-print(list_integers[index])
+#index = sol.find_pivot(list_integers,0,len(list_integers)-1)
+sol.search(list_integers,2)
 
 # print(13%2)
 # print(math.ceil(13/2))
