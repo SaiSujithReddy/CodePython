9bc1a629bfeb5559dd6395cc7edd56f6ddb5f3c8
Sai MarapaReddy <sai.marapareddy@gmail.com>
2019-09-23 12:02:04 -0700

    Update 5. Longest Palindromic Substring.py

--- 5. Longest Palindromic Substring.py
+++ 5. Longest Palindromic Substring.py
@@ -1,3 +1,4 @@
+O(n^3) - Time, with DP O(n^2)
 
 import timeit
 class Solution:
@@ -88,3 +89,29 @@ Things i learnt
 Rather than sorting , use two variables to get the longest palindrome substring
 
 '''
+
+
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        ans = ''
+        max_len = 0
+        n = len(s)
+        DP = [[0] * n for _ in xrange(n)]
+        for i in xrange(n):
+            DP[i][i] = True
+            max_len = 1
+            ans = s[i]
+        for i in xrange(n-1):
+            if s[i] == s[i+1]:
+                DP[i][i+1] = True
+                ans = s[i:i+2]
+                max_len = 2
+        for j in xrange(n):
+            for i in xrange(0, j-1):
+                if s[i] == s[j] and DP[i+1][j-1]:
+                    DP[i][j] = True
+                    if max_len < j - i + 1:
+                        ans = s[i:j+1]
+                        max_len = j - i + 1
+        return ans
