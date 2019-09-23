0cb0f9418bb25c3a8a19fe9788c85da6badd89b4
Sai MarapaReddy <sai.marapareddy@gmail.com>
2019-09-23 11:10:06 -0700

    Update 3. Longest Substring Without Repeating Characters.py

--- 3. Longest Substring Without Repeating Characters.py
+++ 3. Longest Substring Without Repeating Characters.py
@@ -1,27 +1,54 @@
 class Solution:
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        if len(s) == 0:
+    def lengthOfLongestSubstring(self, string):
+        
+        
+        if len(string) == 0:
+            
             return 0
-
-        set_string = set()
-        temp_longest_count =0
-        longest_count =0
-        for x in s:
-            if x in set_string:
-                if temp_longest_count > longest_count:
-                    longest_count = temp_longest_count
-                    temp_longest_count = 1
-                else:
-                    temp_longest_count = 1
+        len_string  = 1
+        list_string = []
+        for x in string:
+            
+            if x not in list_string:
+                list_string.append(x)
             else:
-                set_string.add(x)
-                temp_longest_count += 1
+                
+                #print(list_string)
+                i = list_string.index(x)
+                if len(list_string) > len_string:
+                    len_string = len(list_string)
+                list_string = list_string[i+1:]
+                list_string.append(x)
+                #print(list_string)
+            if len(list_string) > len_string:
+                len_string = len(list_string)
+                #print(list_string)
+        return len_string
+    
+    
+#     def lengthOfLongestSubstring(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
+#         if len(s) == 0:
+#             return 0
 
-        front_longest = longest_count if longest_count > temp_longest_count else temp_longest_count
+#         set_string = set()
+#         temp_longest_count =0
+#         longest_count =0
+#         for x in s:
+#             if x in set_string:
+#                 if temp_longest_count > longest_count:
+#                     longest_count = temp_longest_count
+#                     temp_longest_count = 1
+#                 else:
+#                     temp_longest_count = 1
+#             else:
+#                 set_string.add(x)
+#                 temp_longest_count += 1
+
+#         front_longest = longest_count if longest_count > temp_longest_count else temp_longest_count
 
 
 sol = Solution()
